36d268b7e8a20d7f6863cfa90d74e37ff5f1cd34
Johan Norberg <norberg.j@gmail.com>
2026-02-06 17:37:45 +0900

    Added mui.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,56 +1,63 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import Header from '../components/Header'
-
-import appCss from '../styles.css?url'
+/// <reference types="vite/client" />
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import {
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRoute,
+} from '@tanstack/react-router'
+import { CacheProvider } from '@emotion/react'
+import { Container, CssBaseline, ThemeProvider } from '@mui/material'
+import createCache from '@emotion/cache'
+import fontsourceVariableRobotoCss from '@fontsource-variable/roboto?url'
+import React from 'react'
+import { theme } from '@/setup/theme'
+import { Header } from '@/components/Header'
 
 export const Route = createRootRoute({
   head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-    links: [
-      {
-        rel: 'stylesheet',
-        href: appCss,
-      },
-    ],
+    links: [{ rel: 'stylesheet', href: fontsourceVariableRobotoCss }],
   }),
-
-  shellComponent: RootDocument,
+  component: RootComponent,
 })
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function Providers({ children }: { children: React.ReactNode }) {
+  const emotionCache = createCache({ key: 'css' })
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html>
       <head>
         <HeadContent />
       </head>
       <body>
-        <Header />
-        {children}
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
+        <Providers>
+          <Header />
+
+          <Container component="main" sx={{ paddingBlock: 4 }}>
+            {children}
+          </Container>
+        </Providers>
+
+        <TanStackRouterDevtools position="bottom-right" />
         <Scripts />
       </body>
     </html>
