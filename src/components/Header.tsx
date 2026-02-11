29352a03ee30a3d5e0f784a5763347b801504120
Johan Norberg <norberg.j@gmail.com>
2026-02-11 12:57:11 +0900

    Size of header fix.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import {
   AppBar,
   Avatar,
   Box,
+  Container,
   IconButton,
   Menu,
   MenuItem,
@@ -53,96 +54,98 @@ export function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
-        <Toolbar sx={{ gap: 2 }}>
-          <StyledCustomLink to="/">
-            <img src={Logo} alt="Roadar" width="24" height="24" />
-          </StyledCustomLink>
-          <StyledCustomLink to="/routes">Routes</StyledCustomLink>
-          {/* Search bar, only show if not on home page */}
-          <Box sx={{ display: 'flex', flexGrow: 1, minWidth: 0, mx: 2 }}>
-            {!isHome && (
-              <RouteSearchBar
-                routes={routes}
-                sx={{
-                  flexGrow: 1,
-                  width: '100%',
-                  minWidth: 0,
-                  '& .MuiOutlinedInput-root': {
-                    border: '1px solid',
-                    borderColor: 'grey.300',
-                    backgroundColor: 'white',
-                    height: 44,
-                    // '& fieldset': {
-                    //   border: 'none',
-                    // },
-                  },
-                }}
-              />
-            )}
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 0,
-              display: 'flex',
-              gap: 2,
-              alignItems: 'center',
-              minWidth: 0,
-            }}
-          >
-            {user ? (
-              <>
-                <StyledCustomLink to="/saved">Saved</StyledCustomLink>
-                <StyledCustomLink to="/routes/my">My Routes</StyledCustomLink>
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <Avatar
-                    src={user.picture_url}
-                    alt={user.name}
-                    sx={{ width: 32, height: 32 }}
-                  >
-                    {user.picture_url ? undefined : user.name.charAt(0)}
-                  </Avatar>
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
+        <Container>
+          <Toolbar sx={{ gap: 2 }}>
+            <StyledCustomLink to="/">
+              <img src={Logo} alt="Roadar" width="24" height="24" />
+            </StyledCustomLink>
+            <StyledCustomLink to="/routes">Routes</StyledCustomLink>
+            {/* Search bar, only show if not on home page */}
+            <Box sx={{ display: 'flex', flexGrow: 1, minWidth: 0, mx: 2 }}>
+              {!isHome && (
+                <RouteSearchBar
+                  routes={routes}
+                  sx={{
+                    flexGrow: 1,
+                    width: '100%',
+                    minWidth: 0,
+                    '& .MuiOutlinedInput-root': {
+                      border: '1px solid',
+                      borderColor: 'grey.300',
+                      backgroundColor: 'white',
+                      height: 44,
+                      // '& fieldset': {
+                      //   border: 'none',
+                      // },
+                    },
                   }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={handleClose}
-                >
-                  <MenuItem disabled>
-                    <Typography variant="body1">{user.name}</Typography>
-                  </MenuItem>
-                  <MenuItem
-                    onClick={handleClose}
-                    component={Link}
-                    to="/settings"
+                />
+              )}
+            </Box>
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: 'flex',
+                gap: 2,
+                alignItems: 'center',
+                minWidth: 0,
+              }}
+            >
+              {user ? (
+                <>
+                  <StyledCustomLink to="/saved">Saved</StyledCustomLink>
+                  <StyledCustomLink to="/routes/my">My Routes</StyledCustomLink>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
                   >
-                    Settings
-                  </MenuItem>
-                  <MenuItem to="/signout" component={Link}>
-                    Sign Out
-                  </MenuItem>
-                </Menu>
-              </>
-            ) : (
-              <StyledCustomLink to="/signin">Sign In</StyledCustomLink>
-            )}
-          </Box>
-        </Toolbar>
+                    <Avatar
+                      src={user.picture_url}
+                      alt={user.name}
+                      sx={{ width: 32, height: 32 }}
+                    >
+                      {user.picture_url ? undefined : user.name.charAt(0)}
+                    </Avatar>
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <MenuItem disabled>
+                      <Typography variant="body1">{user.name}</Typography>
+                    </MenuItem>
+                    <MenuItem
+                      onClick={handleClose}
+                      component={Link}
+                      to="/settings"
+                    >
+                      Settings
+                    </MenuItem>
+                    <MenuItem to="/signout" component={Link}>
+                      Sign Out
+                    </MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <StyledCustomLink to="/signin">Sign In</StyledCustomLink>
+              )}
+            </Box>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   )
